633da273fe5adce02ba41b38f09c036797be878b
Brandon Hess <brandon.hess43@gmail.com>
2023-10-12 13:53:31 -0500

    added questions and organized

--- db_tables_exercises.sql
+++ db_tables_exercises.sql
@@ -1,38 +1,43 @@
-SELECT * FROM mysql.user;
+-- Open MySQL Workbench and login to the database server
+-- Save your work in a file named db_tables_exercises.sql
+
+-- List all the databases
 show databases;
+
+-- Write the SQL code necessary to use the albums_db database
 USE albums_db;
+
+-- Show the currently selected database
 select database ();
-use 311_data;
-select database();
+
+-- List all tables in the database
 show tables;
-describe source;
-Show create table source;
-describe source;
-show tables;
-show databases;
-use codeup_db_server;
-show databases;
-leave database;
-select codeup db server;
-select database();
-show databases;
-select database();
-use albums_db;
-select database ();
-show tables;
+
+-- Write the SQL code to switch to the employees database
 use employees;
-select database();
+
+-- Show the currently selected database
+select database ();
+
+-- List all tables in the database
 show tables;
+
+-- Explore the employees table. What different data types are present in this table?
+describe employees;
+
 -- Which table(s) do you think contain a numeric type column?
 		-- departments, dept_emp, dept_manager, employees, salaries, titles
+        
 -- Which table(s) do you think contain a string type column?
 		-- departments, employees, titles
+        
 -- Which table(s) do you think contain a data type column?
 		-- dept_emp, dept_manager, employees, salaries, titles
+        
 -- What is the relationship between the employees and the departments tables?
 		-- Not much, they both have data that ends up in dept_manager?
 
-select dept_manager;
+-- Show the SQL code that created the dept_manager table. Write the SQL it takes to show this as your exercise solution.
 show create table dept_manager; 
 # CREATE TABLE `dept_manager` (
 #	`emp_no` int NOT NULL,
